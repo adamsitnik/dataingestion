d6e2758991ef0f995245e4610bafad8e8cd390e0
Adam Sitnik <adam.sitnik@gmail.com>
2025-07-28 16:27:20 +0200

    DocumentReader abstraction with Azure Document Intelligence initial implementation

---
+++ src/Microsoft.Extensions.DataIngestion.Abstractions/DocumentReader.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DataIngestion
+{
+    public abstract class DocumentReader
+    {
+        public abstract Task<Document> ReadAsync(Stream stream, CancellationToken cancellationToken = default);
+
+        public virtual async Task<Document> ReadAsync(string filePath, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            // There is no need to buffer the file stream when reading a document.
+            // Specifying 1 as buffer size will disable buffering on every target framework (Core and Full).
+            const int DisableBuffering = 1;
+            // By default, FileStream is opened for synchronous I/O operations, async is on demand.
+            const bool useAsync = true;
+
+            using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, DisableBuffering, useAsync);
+            return await ReadAsync(fileStream, cancellationToken);
+        }
+
+        public virtual async Task<Document> ReadAsync(Uri source, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source.IsFile)
+            {
+                return await ReadAsync(source.LocalPath, cancellationToken);
+            }
+
+            HttpClient httpClient = new();
+            using HttpResponseMessage response = await httpClient.GetAsync(source, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            using Stream stream = await response.Content.ReadAsStreamAsync();
+            return await ReadAsync(stream, cancellationToken);
+        }
+    }
+}
---
+++ test/Microsoft.Extensions.DataIngestion.Tests/AzureDocInt/DocumentIntelligenceReaderTests.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+
+using Azure;
+using Azure.AI.DocumentIntelligence;
+using System;
+
+namespace Microsoft.Extensions.DataIngestion.Tests
+{
+    public abstract class DocumentIntelligenceReaderTests : DocumentReaderConformanceTests
+    {
+        protected abstract string ModelName { get; }
+
+        protected override DocumentReader CreateDocumentReader()
+        {
+            string key = Environment.GetEnvironmentVariable("AZURE_DOCUMENT_INT_KEY")!;
+            string endpoint = Environment.GetEnvironmentVariable("AZURE_DOCUMENT_INT_ENDPOINT")!;
+
+            AzureKeyCredential credential = new(key);
+            DocumentIntelligenceClient client = new(new Uri(endpoint), credential);
+
+            return new DocumentIntelligenceReader(client, ModelName);
+        }
+    }
+
+    public sealed class DocumentIntelligenceReaderLayoutModel : DocumentIntelligenceReaderTests
+    {
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/overview?view=doc-intel-4.0.0#layout
+        /// </summary>
+        protected override string ModelName => "prebuilt-layout";
+    }
+
+    public sealed class DocumentIntelligenceReaderReadModel : DocumentIntelligenceReaderTests
+    {
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/overview?view=doc-intel-4.0.0#read
+        /// </summary>
+        protected override string ModelName => "prebuilt-read";
+    }
+}
