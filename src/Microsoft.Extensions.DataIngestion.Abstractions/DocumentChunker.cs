4ac1d2cb29d3c4104bfb0d2cb7f9478cdfdd6377
Krystof Suchanek <53654296+KrystofS@users.noreply.github.com>
2025-08-14 00:26:27 +0200

    Document chunker abstraction

---
+++ src/Microsoft.Extensions.DataIngestion.Abstractions/DocumentChunker.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DataIngestion
+{
+    public abstract class DocumentChunker
+    {
+        public abstract ValueTask<IEnumerable<Chunk>> ProcessAsync(Document document, CancellationToken cancellationToken = default);
+        public abstract ValueTask<IEnumerable<Chunk>> ProcessAsync(List<Document> document, CancellationToken cancellationToken = default);
+    }
+}
