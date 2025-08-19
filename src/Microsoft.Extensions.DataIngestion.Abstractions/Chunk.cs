ecf60d3ef39809ed0aea7f77552029272fc211d0
Krystof Suchanek <53654296+KrystofS@users.noreply.github.com>
2025-08-19 11:41:53 +0200

    support multiple chunk locations

--- src/Microsoft.Extensions.DataIngestion.Abstractions/Chunk.cs
+++ src/Microsoft.Extensions.DataIngestion.Abstractions/Chunk.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Microsoft.Extensions.DataIngestion
 {
@@ -11,22 +13,27 @@ namespace Microsoft.Extensions.DataIngestion
     {
         public string Content { get; }
         /// <summary>
-        /// Chunk location relative to the source document.
+        /// Chunk locations relative to the source document.
         /// </summary>
-        public ChunkLocation Location { get; }
+        public IReadOnlyCollection<ChunkLocation> Locations { get => _locations; }
         public int TokenCount { get; }
 
         public string? Context { get; }
 
-        public Chunk(string content, ChunkLocation location, int tokenCount, string? context = null)
+        private SortedSet<ChunkLocation> _locations;
+        private static readonly ChunkLocationComparer _locationComparer = new ChunkLocationComparer();
+
+        public Chunk(string content, IEnumerable<ChunkLocation> locations, int tokenCount, string? context = null)
         {
             if (string.IsNullOrWhiteSpace(content))
                 throw new ArgumentException("Content cannot be null or whitespace.", nameof(content));
             if (tokenCount <= 0)
                 throw new ArgumentOutOfRangeException(nameof(tokenCount), "Token count must be greater than zero.");
+            if (!locations.Any())
+                throw new ArgumentException("At least one chunk location must be provided.", nameof(locations));
 
             Content = content;
-            Location = location;
+            _locations = new SortedSet<ChunkLocation>(locations, _locationComparer);
             TokenCount = tokenCount;
             Context = context;
         }
@@ -35,7 +42,9 @@ namespace Microsoft.Extensions.DataIngestion
     public struct ChunkLocation
     {
         public int Start { get; }
+        public int End => Start + Length;
         public int Length { get; }
+
 
         public ChunkLocation(int start, int length)
         {
@@ -45,6 +54,18 @@ namespace Microsoft.Extensions.DataIngestion
                 throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be negative.");
             Start = start;
             Length = length;
+        }
+    }
+
+    sealed class ChunkLocationComparer : IComparer<ChunkLocation>
+    {
+        public int Compare(ChunkLocation x, ChunkLocation y)
+        {
+            if (x.Start != y.Start)
+            {
+                return x.Start.CompareTo(y.Start);
+            }
+            return x.Length.CompareTo(y.Length);
         }
     }
 }
