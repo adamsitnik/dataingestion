de4b184b175e5a0d7f35ee1e8e79d035f2ad0f39
Adam Sitnik <adam.sitnik@gmail.com>
2025-08-20 16:30:50 +0200

    introduce DocumentWriter abstraction, provide first impl using MEVD

---
+++ src/Microsoft.Extensions.DataIngestion.Abstractions/DocumentWriter.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DataIngestion;
+
+public abstract class DocumentWriter : IDisposable
+{
+    public abstract Task WriteAsync(Document document, List<Chunk> chunks, CancellationToken cancellationToken = default);
+
+    public abstract void Dispose();
+}
---
+++ src/Microsoft.Extensions.DataIngestion/VectorStoreWriter.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.VectorData;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DataIngestion;
+
+public class VectorStoreWriter<TKey, TRecord> : DocumentWriter
+    where TKey : notnull
+    where TRecord : class
+{
+    private readonly VectorStoreCollection<TKey, TRecord> _vectorStoreCollection;
+    private readonly Func<Document, Chunk, TRecord> _mapper;
+    private bool _existsChecked = false;
+
+    public VectorStoreWriter(VectorStoreCollection<TKey, TRecord> vectorStoreCollection, Func<Document, Chunk, TRecord> mapper)
+    {
+        _vectorStoreCollection = vectorStoreCollection ?? throw new ArgumentNullException(nameof(vectorStoreCollection));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public override void Dispose() => _vectorStoreCollection.Dispose();
+
+    public override async Task WriteAsync(Document document, List<Chunk> chunks, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (!_existsChecked)
+        {
+            await _vectorStoreCollection.EnsureCollectionExistsAsync(cancellationToken).ConfigureAwait(false);
+
+            _existsChecked = true;
+        }
+
+        foreach (Chunk chunk in chunks)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            TRecord record = _mapper(document, chunk);
+
+            await _vectorStoreCollection.UpsertAsync(record, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
