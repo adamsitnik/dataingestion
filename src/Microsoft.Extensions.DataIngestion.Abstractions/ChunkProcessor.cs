5d8a923cacff7786ba058ca26c3ebcbb4e8e65bb
Adam Sitnik <adam.sitnik@gmail.com>
2025-09-15 14:51:25 +0200

    introduce ChunkProcessor

--- src/Microsoft.Extensions.DataIngestion.Abstractions/ChunkProcessor.cs
+++ src/Microsoft.Extensions.DataIngestion.Abstractions/ChunkProcessor.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,9 +8,7 @@ using System.Threading.Tasks;
 namespace Microsoft.Extensions.DataIngestion;
 
 // design notes: it could become an interface if using abstract class does not bring any value.
-public abstract class DocumentWriter : IDisposable
+public abstract class ChunkProcessor
 {
-    public abstract Task WriteAsync(Document document, List<Chunk> chunks, CancellationToken cancellationToken = default);
-
-    public abstract void Dispose();
+    public abstract Task<List<Chunk>> ProcessAsync(List<Chunk> chunks, CancellationToken cancellationToken = default);
 }
