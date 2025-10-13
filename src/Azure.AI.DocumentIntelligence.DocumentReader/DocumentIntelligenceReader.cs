71fc8b5d04e101e310bdd43c680038b8510bfbca
Adam Sitnik <adam.sitnik@gmail.com>
2025-10-13 11:38:04 +0200

    introduce Azure.AI.DocumentIntelligence.DocumentReader

--- src/Azure.AI.DocumentIntelligence.DocumentReader/DocumentIntelligenceReader.cs
+++ src/Azure.AI.DocumentIntelligence.DocumentReader/DocumentIntelligenceReader.cs
@@ -42,8 +42,13 @@ public sealed class DocumentIntelligenceReader : IngestionDocumentReader
             throw new ArgumentNullException(nameof(identifier));
         }
 
+#if NET
         byte[] bytes = await File.ReadAllBytesAsync(source.FullName, cancellationToken);
         BinaryData binaryData = BinaryData.FromBytes(bytes);
+#else
+        using FileStream stream = new(source.FullName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 1, FileOptions.Asynchronous);
+        BinaryData binaryData = await BinaryData.FromStreamAsync(stream, cancellationToken);
+#endif
         return await ReadAsync(new AnalyzeDocumentOptions(_modelName, binaryData), identifier, cancellationToken);
     }
 
@@ -287,7 +292,11 @@ public sealed class DocumentIntelligenceReader : IngestionDocumentReader
             StringBuilder stringBuilder = new(length);
             foreach (var span in spans)
             {
-                stringBuilder.Append(entireContent.AsSpan(span.Offset, span.Length));
+                stringBuilder.Append(entireContent.AsSpan(span.Offset, span.Length)
+#if !NET
+                    .ToString()
+#endif
+                );
             }
             return stringBuilder.ToString();
         }
